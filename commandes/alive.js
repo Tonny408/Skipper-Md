4820e261921a894c980dd97e05a16e702e024286
Brashokish <130168063+Brashokish@users.noreply.github.com>
2024-03-17 12:25:24 +0300

    alive.js

--- commandes/alive.js
+++ commandes/alive.js
@@ -43,7 +43,7 @@ const date = moment().format('DD/MM/YYYY');
  ${message}
  
  
- *FLASH-MD by France King is alive*`
+ *SKIPPER-MD by Skipper 408 is alive*`
 
  if (lien.match(/\.(mp4|gif)$/i)) {
     try {
