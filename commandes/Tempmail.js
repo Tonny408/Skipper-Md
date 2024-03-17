040e9ff68d10dc8ee90c48c21e127320fe81270c
Brashokish <130168063+Brashokish@users.noreply.github.com>
2024-03-17 12:29:59 +0300

    Update Tempmail.js

--- commandes/Tempmail.js
+++ commandes/Tempmail.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "tempmail", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*FLASH-MD* Temporary emails,Powered by *France King* \n\n ' + "Click the link below to create unlimited TEMPORARY Emails. Powered by *FLASH-MD.*";
+    let z = '*SKIPPER-MD* Temporary emails,Powered by *Tonny 408* \n\n ' + "Click the link below to create unlimited TEMPORARY Emails. Powered by *FLASH-MD.*";
     let d = ' https://extraclass.ng';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/f1f2303ff4e39b0a3b6b3.jpg';
@@ -19,8 +19,8 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *FLASH-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *France King*'
+   let z ='Salut je m\'appelle *SKIPPER-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *Tonny 408*'
       let varmess=z+d
       var img='https://telegra.ph/file/13d63c21c1a665bfd8324.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
