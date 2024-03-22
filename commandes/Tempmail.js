f6adbae362406009f3be75d52fcfffa933e472e7
Brashokish <130168063+Brashokish@users.noreply.github.com>
2024-03-22 23:44:07 +0300

    Update Tempmail.js

--- commandes/Tempmail.js
+++ commandes/Tempmail.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "tempmail", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*SKIPPER-MD* Temporary emails,Powered by *Tonny 408* \n\n ' + "Click the link below to create unlimited TEMPORARY Emails. Powered by *FLASH-MD.*";
+    let z = '*SKIPPER-MD* Temporary emails,Powered by *Tonny 408* \n\n ' + "Click the link below to create unlimited TEMPORARY Emails. Powered by *SKIPPER-MD.*";
     let d = ' https://extraclass.ng';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/f1f2303ff4e39b0a3b6b3.jpg';
