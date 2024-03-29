28c156312933bd2a8079533af7e88c83a185f983
MG TOPHAZ <130512949+MGTOPHAZ@users.noreply.github.com>
2024-03-30 01:21:22 +0300

    Skipper-MD 🦄
    Forwarded many times 🦄

---
+++ Commands/botTest.js
@@ -0,0 +1,20 @@
+const {skipper } = require("../framework/skipper");
+const moment = require("moment-timezone");
+const { default: axios } = require('axios');
+//const conf = require('../set');
+
+
+skipper({ nomCom: 'test',
+    desc: 'To test the bot',
+    Categorie: 'General',
+    reaction: '🐐', 
+    fromMe: 'true', 
+
+       
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+    return repondre('*SKIPPER-MD is Alive!!!!!!!*\n ```' + powered by + '``` *Tonny 408*```') 
+  }
+)
+ 
