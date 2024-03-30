3474572f9b9d34400e46326a4066f0614967ab8c
MG TOPHAZ <130512949+MGTOPHAZ@users.noreply.github.com>
2024-03-31 00:10:09 +0300

    SKIPPER-MD 🍷Xx 🦄
    Forwarded many times 🦄 📡

---
+++ commandes/quote.js
@@ -0,0 +1,49 @@
+const { skipper } = require('../framework/skipper');
+
+skipper({ nomCom: 'quote', categorie: 'Fun' }, async (dest, zk, commandeOptions) => {
+  const { ms, repondre, verifGroupe, arg } = commandeOptions;
+  if (!verifGroupe) {
+    repondre('Commande réservée au groupe uniquement');
+    return;
+  }
+
+  if (!arg[0]) {
+    try {
+      fetch('https://animechan.xyz/api/random')
+        .then((response) => response.json())
+        .then(async (quote) => {
+          repondre(`╔══════════════════════════╗
+║   SKIPPPER-MD              ║
+╚══════════════════════════╝
+
+🎬 Anime: ${quote.anime}
+👤 Character: ${quote.character}
+💬 Quote: ${quote.quote}
+
+Powered by *Tonny 408*`);
+        });
+    } catch (e) {
+      repondre('Erreur lors de la génération de la citation : ' + e.message);
+    }
+  } else {
+    const query = arg.join(' ');
+
+    try {
+      fetch('https://animechan.xyz/api/random/character?name=' + query)
+        .then((response) => response.json())
+        .then(async (quote) => {
+          repondre(`╔══════════════════════════╗
+║   SKIPPER-MD               ║
+╚══════════════════════════╝
+
+🎬 Anime: ${quote.anime}
+👤 Character: ${quote.character}
+💬 Quote: ${quote.quote}
+
+Powered by SKIPPER-MD`);
+        });
+    } catch (e) {
+      repondre('Erreur lors de la génération de la citation : ' + e.message);
+    }
+  }
+});
---
+++ commandes/test.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { skipper } = require("../framework/skipper");
+skipper({ nomCom: "repo", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = 'Hello This is  *SKIPPER-MD* \n\n ' + "The Following is *SKIPPER-MD Repo.*";
+    let d = ' https://github.com/Tonny408/Skipper-Md';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/89bfd1ddc221647f3c7ce.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+/*module.exports.commande = () => {
+  var nomCom = ["test","t"]
+  var reaction="☺️"
+  return { nomCom, execute,reaction }
+};
+
+async function  execute  (origineMessage,zok) {
+  console.log("Commande saisie !!!s")
+   let z ='Salut je m\'appelle *SKIPPER-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *Tonny 408*'
+      let varmess=z+d
+      var img='https://telegra.ph/file/89bfd1ddc221647f3c7ce.jpg';
+await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
+}  */ 
