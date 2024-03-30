d047e0c431ac682287f352004033e947880078dc
Brashokish <130168063+Brashokish@users.noreply.github.com>
2024-03-31 01:03:27 +0300

    𝙎𝙠𝙞𝙥𝙥𝙚𝙧 𝙈𝙙

--- commandes/AI.js
+++ commandes/AI.js
@@ -1,4 +1,4 @@
-const { skipper } = require('../framework/skipper');
+lconst { skipper } = require('../framework/skipper');
 const traduire = require("../framework/traduction") ;
 const { default: axios } = require('axios');
 //const conf = require('../set');
@@ -55,7 +55,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
   
       // Regrouper les arguments en une seule chaîne séparée par "-"
       const image = arg.join(' ');
-      const response = await axios.get(`https://vihangayt.me/tools/photoleap?q=${image}`);
+      const response = await axios.get(`https://api.maher-zubair.tech/ai/chatgptv4?q=${image}`);
       
       const data = response.data;
       let caption = '*powered by SKIPPER-MD*';
@@ -83,7 +83,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
   
       // Regrouper les arguments en une seule chaîne séparée par "-"
       const question = arg.join(' ');
-      const response = await axios.get(`https://vihangayt.me/tools/photoleap?q=${question}`);
+      const response = await axios.get(`https://api.maher-zubair.tech/ai/chatgptv4?q=${question}`);
       
       const data = response.data;
       if (data) {
